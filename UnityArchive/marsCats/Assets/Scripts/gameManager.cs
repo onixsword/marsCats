116e1e2c20591c4ee9f3bf7cc165c47dce8756f4
Deus <ultima_lux@hotmail.com>
2019-05-06 03:45:36 -0700

    Correcciones menores
    Eliminado carpeta de leap motion

--- UnityArchive/marsCats/Assets/Scripts/gameManager.cs
+++ UnityArchive/marsCats/Assets/Scripts/gameManager.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
-
     public static gameManager instance;
+    private Transform player;
+
+    [SerializeField]private float timeToStorm;
+    private Transform storm;
+    private bool stormIsActive;
+
+    private Transform[] edifications;
+
+  
 
     private void Awake()
     {
@@ -13,4 +21,36 @@ public class gameManager : MonoBehaviour
         else gameManager.instance = this;
     }
 
+    private void Start()
+    {
+        StartCoroutine(stormTimer(timeToStorm));
+    }
+
+    private IEnumerator stormTimer(float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            StormIsActive = !StormIsActive;
+        }
+    }
+
+    private void stormChangeState()
+    {
+        storm.gameObject.SetActive(StormIsActive);
+        storm.position = player.position; 
+    }
+
+    public Transform Player { get => player; set => player = value; }
+    public bool StormIsActive {
+        get
+        {
+            return stormIsActive;
+        }
+        set
+        {
+            stormIsActive = value;
+            stormChangeState();
+        }
+    }
 }
