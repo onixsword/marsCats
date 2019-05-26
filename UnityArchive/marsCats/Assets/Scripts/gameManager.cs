6415f517d4e1207a18ff80b00d20c209bb42943a
Deus <ultima_lux@hotmail.com>
2019-05-26 13:47:11 -0700

    Added Weather RNG code

--- UnityArchive/marsCats/Assets/Scripts/gameManager.cs
+++ UnityArchive/marsCats/Assets/Scripts/gameManager.cs
@@ -4,53 +4,57 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    private enum disasterState { sandstorm, solarEruption, none };
     public static gameManager instance;
     private Transform player;
 
-    [SerializeField]private float timeToStorm;
-    private Transform storm;
-    private bool stormIsActive;
+    [Header("Weather")]
 
-    private Transform[] edifications;
+    [SerializeField] private Vector2 mapSize;
+    [SerializeField] private float timeToUpdateWeather;
+    [SerializeField] private Vector2 disasterRangeSize;
+    private Vector2 disasterPosition;
+    private float disasterSize;
+    disasterState activeDisaster;
+    
 
-  
+    [SerializeField] private float sandstormProbability;
+    [SerializeField] private float solarEruptionProbability;
+
+    private List<Transform> edifications;
+    private List<NPC> npcs;
 
     private void Awake()
     {
         if (gameManager.instance != null && gameManager.instance != this) GameObject.Destroy(gameObject);
         else gameManager.instance = this;
+
+        edifications = new List<Transform>();
+        npcs = new List<NPC>();
+
+        activeDisaster = disasterState.none;
     }
 
     private void Start()
     {
-        StartCoroutine(stormTimer(timeToStorm));
+        StartCoroutine(disasterTimer(timeToUpdateWeather));
     }
 
-    private IEnumerator stormTimer(float time)
+    private IEnumerator disasterTimer(float time)
     {
         while (true)
         {
             yield return new WaitForSeconds(time);
-            StormIsActive = !StormIsActive;
+            activeDisaster = Random.Range(0, 100) < sandstormProbability ? disasterState.sandstorm : Random.Range(0, 100) < sandstormProbability + solarEruptionProbability ? disasterState.solarEruption : disasterState.none;
+            if (activeDisaster != disasterState.none)
+            {
+                disasterSize = Random.Range(disasterRangeSize.x, disasterRangeSize.y);
+                disasterPosition = new Vector2(Random.Range(-mapSize.x / 2 + disasterSize, mapSize.x / 2 - disasterSize), Random.Range(-mapSize.y / 2 + disasterSize, mapSize.y / 2 - disasterSize));
+            }
         }
-    }
-
-    private void stormChangeState()
-    {
-        storm.gameObject.SetActive(StormIsActive);
-        storm.position = player.position; 
     }
 
     public Transform Player { get => player; set => player = value; }
-    public bool StormIsActive {
-        get
-        {
-            return stormIsActive;
-        }
-        set
-        {
-            stormIsActive = value;
-            stormChangeState();
-        }
-    }
+    public List<Transform> Edifications { get => edifications; set => edifications = value; }
+    public List<NPC> NPCs { get => npcs; set => npcs = value; }
 }
